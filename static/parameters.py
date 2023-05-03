09177c370b45e746e426a8c8e83a51cd7ffcef42
varxx-v0 <132223124+varxx-v0@users.noreply.github.com>
2023-05-03 15:58:14 -0400

    Add files via upload

---
+++ static/parameters.py
@@ -0,0 +1,17 @@
+CURRENT_SEASON = 2022
+SUPPORTED_SEASONS = [2021, 2022]
+SUPPORTED_STATS_SEASONS = ['Career', 2022, 2021, 2019, 2018, 2017, 2016, 2015,
+    2014, 2013, 2012]
+
+### Team and Player Stats
+
+TEAM_STATS_PER_DICT = {'Total': 'total', 'Per game': 'game'}
+TEAM_STATS_VS_DICT = {'Team': 'team', 'Opponent': 'opponent'}
+PLAYER_STATS_PER_DICT = {'Total': 'total', 'Per game': 'game', 
+     'Per 10 points': 'points', 'Per 10 possessions': 'possessions', 
+     'Per 100 minutes': 'minutes'}
+
+### Throwing 
+
+THROWS_TYPE_DICT = {'Pass': 0, 'Dump': 1, 'Dish': 2, 'Swing': 3, 'Pull': 4, 'Throwaway': 5, 'Huck': 6}
+THROWS_TYPE_DICT_INVERSE = {0: 'Pass', 1: 'Dump', 2: 'Dish', 3: 'Swing', 4: 'Pull', 5: 'Throwaway', 6: 'Huck'}
